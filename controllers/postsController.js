9b35063871d6c71e3723bcf8a434e5d09eaf5ee4
angelostori <angelostori13@gmail.com>
2025-11-26 11:30:22 +0100

    modify posts routes to use postsController methods for better organization and maintainability

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -6,3 +6,60 @@ Come prima cosa, creiamo un controller per i nostri post, in una cartella contro
 All’interno, prepariamo tutte le funzioni necessarie e copiamo in ciascuna la logica delle funzioni che attualmente 
 si trovano nel router (al momento restituiscono solo dei messaggi). 
 */
+
+const index = (req, res) => {
+    res.json(posts)
+}
+
+const show = (req, res) => {
+    const id = Number(req.params.id)
+    const thisPost = posts.find(post => post.id === id)
+    // res.send(`Show the post with id: ${req.params.id}`)
+
+    if (!thisPost) {
+        res.send(`The post with id: ${req.params.id} doesn't exist`)
+    }
+
+    res.json(thisPost)
+}
+
+const store = (req, res) => {
+    res.send('Store a new post here')
+}
+
+const update = (req, res) => {
+    res.send(`Update the post with id: ${req.params.id}`)
+}
+
+const modify = (req, res) => {
+    res.send(`Modify the post with id: ${req.params.id}`)
+}
+
+const destroy = (req, res) => {
+    const id = Number(req.params.id)
+    const thisPost = posts.find(post => post.id === id)
+
+    if (!thisPost) {
+        return res.status(404).json({
+            error: true,
+            message: 'Post not found!'
+        })
+    }
+
+    posts.splice(posts.indexOf(thisPost), 1)
+
+    res.sendStatus(204)
+}
+
+
+const postsController = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+}
+
+
+module.exports = postsController
--- routes/posts.js
+++ routes/posts.js
@@ -1,56 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const posts = require('../assets/posts.js')
+const postsController = require('../controllers/postsController.js')
 
 
 // index
-router.get('/', (req, res) => {
-    res.json(posts)
-})
+router.get('/', postsController.index)
 
 // show
-router.get('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const thisPost = posts.find(post => post.id === id)
-    // res.send(`Show the post with id: ${req.params.id}`)
-
-    if (!thisPost) {
-        res.send(`The post with id: ${req.params.id} doesn't exist`)
-    }
-
-    res.json(thisPost)
-})
+router.get('/:id', postsController.show)
 
 // store
-router.post('/', (req, res) => {
-    res.send('Store a new post here')
-})
+router.post('/', postsController.store)
 
 // update
-router.put('/:id', (req, res) => {
-    res.send(`Update the post with id: ${req.params.id}`)
-})
+router.put('/:id', postsController.update)
 
 // modify (optional)
-router.patch('/:id', (req, res) => {
-    res.send(`Modify the post with id: ${req.params.id}`)
-})
+router.patch('/:id', postsController.modify)
 
 // destroy
-router.delete('/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const thisPost = posts.find(post => post.id === id)
-
-    if (!thisPost) {
-        return res.status(404).json({
-            error: true,
-            message: 'Post not found!'
-        })
-    }
-
-    posts.splice(posts.indexOf(thisPost), 1)
-
-    res.sendStatus(204)
-})
+router.delete('/:id', postsController.destroy)
 
 module.exports = router
