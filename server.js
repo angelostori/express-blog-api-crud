f56d861663b9511fb9f9e9db235906cde81c9d7d
angelostori <angelostori13@gmail.com>
2025-11-30 20:04:54 +0100

    add error handling middleware for 404 and internal server errors

--- server.js
+++ server.js
@@ -16,6 +16,24 @@ app.use(express.json())
 
 app.use('/api/posts', postsRouter)
 
+// MIDDLEWARE 404
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: true,
+        message: 'Endpoint non trovato'
+    });
+});
+
+// MIDDLEWARE DI ERRORE
+app.use((err, req, res, next) => {
+    console.error("Errore interno:", err);
+
+    res.status(500).json({
+        error: true,
+        message: 'Errore interno del server'
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
